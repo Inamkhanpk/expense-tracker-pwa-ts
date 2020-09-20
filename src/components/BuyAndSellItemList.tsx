783286e5fbdaa8cc99067ff6aba10e81d55bd0ea
Inam Khan <inamk2111@gmail.com>
2020-09-20 10:56:18 +0000

    project pushed

--- src/components/BuyAndSellItemList.tsx
+++ src/components/BuyAndSellItemList.tsx
@@ -9,9 +9,9 @@ import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+//import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/IconButton'
+//import IconButton from '@material-ui/core/IconButton'
 const useStyles  = makeStyles((theme) =>({
   root:{
     backgroundColor:theme.palette.info.main,
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
+//import Grid from '@material-ui/core/Grid'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
